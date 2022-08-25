8ccd0078d925e5fe60bc13a586297dd5e8e20cc6
gabrielemigliorinii <97559996+gabrielemigliorinii@users.noreply.github.com>
2022-08-25 13:02:00 +0200

    Update composti.py

--- composti.py
+++ composti.py
@@ -1,4 +1,5 @@
 
+
 class Composto:
 
     __elements = ("H", "He", "Li", "Be", "B", "C", "N", "O", "F", "Ne", "Na", "Mg", "Al", "Si", "P", "S", "Cl", "Ar", "K", "Ca", "Sc", "Ti", "V", "Cr", "Mn", "Fe", "Co", "Ni", "Cu", "Zn", "Ga", "Ge", "As", "Se", "Br", "Kr", "Rb", "Sr", "Y", "Zr", "Nb", "Mo", "Tc", "Ru", "Rh", "Pd", "Ag", "Cd", "In", "Sn", "Sb", "Te", "I", "Xe", "Cs", "Ba", "La", "Ce", "Pr", "Nd", "Pm", "Sm", "Eu", "Gd", "Tb", "Dy", "Ho", "Er", "Tm", "Yb", "Lu", "Hf", "Ta", "W", "Re", "Os", "Ir", "Pt", "Au", "Hg", "Tl", "Pb", "Bi", "Po", "At", "Rn", "Fr", "Ra", "Ac", "Th", "Pa", "U", "Np", "Pu", "Am", "Cm", "Bk", "Cf", "Es", "Fm", "Md", "No", "Lr", "Rf", "Db", "Sg", "Bh", "Hs", "Mt", "Ds", "Rg", "Cn", "Nh", "Fl", "Mc", "Lv", "Ts", "Og")
@@ -71,6 +72,8 @@ class Composto:
                 self.Formula += (str)(obj[1]) if obj[1] > 1 else ""
             return [mm, "g/mol"]
 
+#---------------------------------------------------------------------------------------------------------------------------------------
+
 def App():
     import os
     os.system("CLS")
@@ -89,5 +92,8 @@ def App():
         os.system("PAUSE")
         os.system("CLS")
 
+#---------------------------------------------------------------------------------------------------------------------------------------
+
 if __name__ == "__main__":
     App()
+
